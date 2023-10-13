107d9e60f0156242a35b5cf41f4406d26ce5dd21
bagpyp <robert@bagpyp.net>
2023-10-13 10:55:22 -0700

    little more main refac

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ from src.util.path_utils import DATA_DIR
 def main():
     start_time = dt.datetime.now()
     print(
-        f"Began {start_time}, processing changes in RetailPro over the last {update_window_hours} hours..."
+        f"Began at {start_time}, processing changes in RetailPro over the last {update_window_hours} hours..."
     )
 
     if not apply_changes:
@@ -66,19 +66,23 @@ def main():
     df = df.set_index("sku")
     df.update(pd.read_pickle(f"{DATA_DIR}/media.pkl"))
 
-    if not apply_changes:
-        file_structure_df = pd.read_pickle(f"{DATA_DIR}/fileDf.pkl")
-    else:
+    if apply_changes:
         file_structure_df = build_image_locations_from_file_structure()
+    else:
+        file_structure_df = pd.read_pickle(f"{DATA_DIR}/fileDf.pkl")
 
     df = df.join(file_structure_df)
     df = prepare_df_for_upload(df)
+
     product_payloads_for_update, product_payloads_for_creation = build_payloads(df)
 
     if apply_changes:
         update_products(product_payloads_for_update)
         create_products(product_payloads_for_creation)
         send_to_quivers()
+
+    end_time = dt.datetime.now()
+    print(f"Finished at {end_time}, duration process was {end_time - start_time}")
 
 
 if __name__ == "__main__":
